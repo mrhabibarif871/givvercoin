280e68ad0e6a27bfdaf84a61a06c7ab84e4b34ea
mrhabibarif871 <mrhabibarif871@gmail.com>
2025-05-01 03:11:06 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 const helmet = require("helmet");
 const CORS = require("cors");
-const test = require("./tests/phaser");
-
+const test = require("./tests/phaser")
 const app = express();
 
 // Connect Database
@@ -13,19 +13,27 @@ connectDB();
 app.use(express.json({ extended: false }));
 app.use(helmet());
 app.use(CORS());
-
 // Define Routes
 app.use("/api/users", require("./routes/api/users"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/play", require("./routes/api/play"));
 app.use("/api/task", require("./routes/api/task"));
 
-// Removed frontend serving code to avoid index.html error
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(Server started on port ${PORT}));
 
 module.exports = app;
 
-// Testing server
-// test();
+//Testing server
+//test();
